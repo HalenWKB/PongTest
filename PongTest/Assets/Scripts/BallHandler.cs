e0578d0300cf4cc01ae913b5ab7037748ae967da
Halberd <elhalo63@gmail.com>
2021-08-31 11:57:32 +1000

    Basic Project with ball

---
+++ PongTest/Assets/Scripts/BallHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHandler : MonoBehaviour
+{
+    [SerializeField] private Vector3 m_startVelo;
+    
+    private Vector3 m_velocity;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_velocity = m_startVelo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += m_velocity * Time.deltaTime;
+    }
+}
