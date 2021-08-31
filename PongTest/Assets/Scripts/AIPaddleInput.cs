55e76113503149016fb3875dc07a1f5dacf52c3c
Halberd <elhalo63@gmail.com>
2021-08-31 14:07:59 +1000

    AI now beatable (But still jittery)

--- PongTest/Assets/Scripts/AIPaddleInput.cs
+++ PongTest/Assets/Scripts/AIPaddleInput.cs
@@ -7,16 +7,32 @@ public class AIPaddleInput : MonoBehaviour
 {
     private PaddleHandler m_paddle;
 
+    [SerializeField] private float m_aiReactionTimeMin = 0.2f;
+    [SerializeField] private float m_aiReactionTimeMax = 0.6f;
+
+    [SerializeField] private float m_aiReadInaccuracy = 2f;
+
+    private bool m_currentInputIsLeft = true;
+    
     void Start()
     {
         m_paddle = GetComponent<PaddleHandler>();
+        StartCoroutine(MakeMoveDecision());
+    }
+    
+    IEnumerator MakeMoveDecision()
+    {
+        Vector3 aiReadOfBallPos = Managers.Gameplay.GetBallPos() + (transform.up * Random.Range(-m_aiReadInaccuracy, m_aiReadInaccuracy));
+        m_currentInputIsLeft = HelperFunctions.IsLeftOfOrOnRay2D(aiReadOfBallPos, transform.position, transform.right);
+        
+        float waitTime = Random.Range(m_aiReactionTimeMin, m_aiReactionTimeMax);
+        yield return new WaitForSeconds(waitTime);
+        
+        StartCoroutine(MakeMoveDecision());
     }
     
     void Update()
     {
-        bool lInput = HelperFunctions.IsLeftOfOrOnRay2D(Managers.Gameplay.GetBallPos(), transform.position, transform.right);
-        bool rInput = !lInput;
-        
-        if (lInput || rInput) m_paddle.MoveInput(lInput);
+        m_paddle.MoveInput(m_currentInputIsLeft);
     }
 }
