fa5d0e137b5ce9aa2c43c3196fca9e01b21948fa
Halberd <elhalo63@gmail.com>
2021-08-31 12:30:46 +1000

    Paddle player control

---
+++ PongTest/Assets/Scripts/PaddleHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleHandler : MonoBehaviour
+{
+    [SerializeField] private float m_paddleSpeed;
+    
+    public void MoveInput(bool isLeftInput)
+    {
+        transform.position += transform.up * m_paddleSpeed * Time.deltaTime * (isLeftInput ? -1 : 1);
+    }
+}
---
+++ PongTest/Assets/Scripts/PlayerPaddleInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PaddleHandler))]
+public class PlayerPaddleInput : MonoBehaviour
+{
+    [SerializeField] private KeyCode m_leftKey = KeyCode.W;
+    [SerializeField] private KeyCode m_rightKey = KeyCode.S;
+
+    private PaddleHandler m_paddle;
+
+    void Start()
+    {
+        m_paddle = GetComponent<PaddleHandler>();
+    }
+    
+    void Update()
+    {
+        bool lInput = Input.GetKey(m_leftKey);
+        bool rInput = Input.GetKey(m_rightKey);
+        
+        if ((lInput || rInput) && !(lInput && rInput)) m_paddle.MoveInput(lInput);
+    }
+}
