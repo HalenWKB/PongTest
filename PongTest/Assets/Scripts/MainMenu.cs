ab5a791317bf8bb583aa0f5c1c1a0be59ff2cad8
Halberd <elhalo63@gmail.com>
2021-08-31 16:03:58 +1000

    Menu MVP done

---
+++ PongTest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayPvP() { PlayGame(GameMode.PvP); }
+    public void PlayPvE() { PlayGame(GameMode.PvE); }
+    public void PlayEvE() { PlayGame(GameMode.EvE); }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    void PlayGame(GameMode mode)
+    {
+        Managers.Mode.SetGameMode(mode);
+        SceneManager.LoadScene(1);
+    }
+}
